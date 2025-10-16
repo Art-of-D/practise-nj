64e0b128355e7e70a7628745d7e69919dea3940a
Artur Didur <didur.art@gmail.com>
2025-10-16 19:31:28 +0200

    some tw

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,21 +3,24 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between items-center bg-amber-500 rounded-xl">
-      <Link href="/" className="ml-3">
-        <Image src="/logo.svg" alt="logo" width={50} height={50} />
-      </Link>
-      <nav className="mr-3 pt-4">
-        <ul className="flex flex-row space-between gap-3 font-medium">
-          <li className="hover:underline hover:text-rose-900">
-            <Link href="/community">Community</Link>
-          </li>
-          <li className="hover:underline hover:text-rose-900">
-            <Link href="/meals">Meals</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className="relative h-20">
+      <div className="absolute z-0 inset-0 header-wave-clip  bg-amber-500 "></div>
+      <header className="relative z-10 flex flex-row justify-between items-center mr-10 ml-10">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={64} height={64} />
+        </Link>
+        <nav className="h-20 flex items-center font-[--font-mono]">
+          <ul className="flex flex-row space-between gap-3 text-xl font-bold">
+            <li className="hover:underline hover:text-rose-900">
+              <Link href="/community">Community</Link>
+            </li>
+            <li className="hover:underline hover:text-rose-900">
+              <Link href="/meals">Meals</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
 
